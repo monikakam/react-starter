812f2bfa1ec91e14562fbf0d3ef87712c05598aa
Monika Kamecka <monika.kamecka@gmail.com>
2021-03-17 21:41:23 +0100

    Add changes to App Hero List and Column

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
 const Hero = props => (      
       <header className={styles.component}>        
         <h2 className={styles.title}>{props.titleText}</h2>
-        <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}></img>
+        <img src={props.image} className={styles.image} ></img>
       </header>      
     );
+
+    Hero.propTypes = {
+        titleText: PropTypes.node.isRequired,
+        image: PropTypes.string,
+    }
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node,
+        image: PropTypes.string,
+    }
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>
+    }
   render() {
     return (      
       <section className={styles.component}>        
-        <h2><Hero titleText={this.props.title} /></h2>
+        <h2><Hero titleText={this.props.title} image={this.props.image}/></h2>
         <div className={styles.description}>
             {this.props.children}
         </div>
+        <div className={styles.columns}>
+            <ul>
+                <li>Animals</li>
+                <li>Animals</li>
+                <li>Animals</li>
+            </ul>
+        </div>
+                
       </section>      
     )
   }
