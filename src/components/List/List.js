649f35ad7dec1284dd8743e750a48e312c84343f
Monika Kamecka <monika.kamecka@gmail.com>
2021-03-17 13:56:08 +0100

    Add modified App Hero and List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,7 +6,10 @@ class List extends React.Component {
   render() {
     return (      
       <section className={styles.component}>        
-        <h2><Hero /></h2>
+        <h2><Hero titleText={this.props.title} /></h2>
+        <div className={styles.description}>
+            {this.props.children}
+        </div>
       </section>      
     )
   }
