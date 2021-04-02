bb39bb0ee6d2bc22b6096f71932d0b7baf17c7b3
Monika Kamecka <monika.kamecka@gmail.com>
2021-04-02 15:28:59 +0200

    Add changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styles from './App.scss';
 import List from '../List/ListContainer';
 import PropTypes from 'prop-types';
 //import {pageContents, listData} from '../../data/dataStore';
+import Search from '../Search/SearchContainer.js';
 
 class App extends React.Component {
   static propTypes = {
@@ -16,7 +17,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        
+        <Search />
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
